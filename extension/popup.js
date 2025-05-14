4a1ef1ddb5d81b72163282cc6c1746577a9a01b8
Rahul <rahulpedapudi.01@gmail.com>
2025-05-14 22:55:25 +0530

    add style, fix minor problems

--- extension/popup.js
+++ extension/popup.js
@@ -1,18 +1,34 @@
-﻿document
-  .getElementById("submit-btn")
-  .addEventListener("click", async (event) => {
-    const data = { message: document.getElementById("input").value };
+﻿document.getElementById("submit-btn").addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs && tabs.length > 0) {
+      const activeTab = tabs[0];
+      const tabId = activeTab.id;
+      const tabUrl = activeTab.url;
+      const tabTitle = activeTab.title;
 
-    try {
-      const res = await fetch("http://localhost:5000/api/save", {
+      tabData = {
+        id: tabId,
+        url: tabUrl,
+        title: tabTitle,
+      };
+
+      fetch("http://localhost:5000/api/save", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-      });
-      const result = await res.json();
-      document.getElementById("response").innerText =
-        "✅ " + JSON.stringify(result);
-    } catch (error) {
-      document.getElementById("response").innerText = "Failed " + error.message;
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(tabData),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log("✅ Data saved:", result);
+          document.getElementById("response").innerHTML =
+            JSON.stringify(result);
+        })
+        .catch((error) => {
+          console.error("❌ Error saving data:", error);
+          document.getElementById("response").innerHTML = error.message;
+        });
     }
   });
+});
