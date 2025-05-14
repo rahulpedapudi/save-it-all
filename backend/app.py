cb7d028491cc4483d145a4b7cffb7dba6693ad40
Rahul <rahulpedapudi.01@gmail.com>
2025-05-14 18:35:01 +0530

    init-commit

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+﻿from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/api/save", methods=["POST"])
+def save_data():
+    data = request.json
+    print("Data Received", data)
+    return jsonify({"status": "success", "received": data}), 200
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ extension/popup.js
@@ -0,0 +1,18 @@
+﻿document
+  .getElementById("submit-btn")
+  .addEventListener("click", async (event) => {
+    const data = { message: document.getElementById("input").value };
+
+    try {
+      const res = await fetch("http://localhost:5000/api/save", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      const result = await res.json();
+      document.getElementById("response").innerText =
+        "✅ " + JSON.stringify(result);
+    } catch (error) {
+      document.getElementById("response").innerText = "Failed " + error.message;
+    }
+  });
