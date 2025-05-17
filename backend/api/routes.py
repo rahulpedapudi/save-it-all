d1038272ea92e1ea34f59beaefee8b43db08b52e
Rahul <rahulpedapudi.01@gmail.com>
2025-05-18 00:10:06 +0530

    add delete functionality

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -29,7 +29,6 @@ def save_data():
 @api_bp.route("/links", methods=["GET"])
 def get_links():
     mongo = current_app.mongo
-    # links = list(mongo.db.links.find({}, {"_id": 0}))
     links = list(mongo.db.links.find({}))
     # Convert _id to string
     for link in links:
@@ -42,11 +41,18 @@ def get_analyzed_post(id):
     pass
 
 
-@api_bp.route("/links/<id>")
-def get_post(id):
+@api_bp.route("/links/<id>", methods=["GET", "DELETE"])
+def handle_link(id):
     mongo = current_app.mongo
-    link = mongo.db.links.find_one({"_id": ObjectId(id)})
-    if link:
-        link["_id"] = str(link["_id"])
-        return jsonify(link)
-    return jsonify({"error": "Link not Found!"}), 404
+    if request.method == 'GET':
+        link = mongo.db.links.find_one({"_id": ObjectId(id)})
+        if link:
+            link["_id"] = str(link["_id"])
+            return jsonify(link)
+        return jsonify({"error": "Link not Found!"}), 404
+    if request.method == 'DELETE':
+        res = mongo.db.links.delete_one({"_id": ObjectId(id)})
+        if res.deleted_count == 1:
+            return jsonify({"message": "deleted successfully"}), 200
+        else:
+            return jsonify({"error": "Link not found"}), 404
