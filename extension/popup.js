b59e0420793db30b79d10a5e30e5dd8dd204693c
Rahul <rahulpedapudi.01@gmail.com>
2025-05-22 19:46:17 +0530

    chore: remove vanillaJS in favor of React

--- extension/popup.js
+++ extension/popup.js
@@ -1,34 +1,52 @@
-﻿document.getElementById("submit-btn").addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    if (tabs && tabs.length > 0) {
-      const activeTab = tabs[0];
-      const tabId = activeTab.id;
-      const tabUrl = activeTab.url;
-      const tabTitle = activeTab.title;
+﻿let currentTab = null;
 
-      tabData = {
-        id: tabId,
-        url: tabUrl,
-        title: tabTitle,
-      };
+document.getElementById("close-btn").addEventListener("click", () => {
+  window.close();
+});
 
-      fetch("http://localhost:5000/api/save", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(tabData),
-      })
-        .then((response) => response.json())
-        .then((result) => {
-          console.log("✅ Data saved:", result);
-          document.getElementById("response").innerHTML =
-            JSON.stringify(result);
-        })
-        .catch((error) => {
-          console.error("❌ Error saving data:", error);
-          document.getElementById("response").innerHTML = error.message;
-        });
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+
+    currentTab = tab;
+
+    document.getElementById("url-display").textContent = tab.url;
+    document.getElementById("title-display").textContent = tab.title;
+  } catch (error) {
+    console.error("Failed to get tab: ", error);
+  }
+});
+
+document.getElementById("primary-btn").addEventListener("click", async () => {
+  if (!currentTab) return;
+
+  const payload = {
+    id: currentTab.id,
+    title: currentTab.title,
+    url: currentTab.url,
+    // Add additional data like tags, notes, etc.
+  };
+
+  try {
+    const res = fetch("http://localhost:5000/api/save", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
+    const json = await res.json();
+    console.log("Saved!", json);
+    if (res.ok) {
+      // Redirect to success.html
+      window.location.href = "success.html";
+    } else {
+      console.error("Save failed.");
     }
-  });
+  } catch (error) {
+    console.error("Failed to Save: ", error);
+  }
 });
